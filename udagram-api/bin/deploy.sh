25ae526634a4c397086e0f249320a52a3ddd7ed1
Mersal5674 <82838420+Mersal5674@users.noreply.github.com>
2022-03-24 16:42:02 +0200

    new

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -2,4 +2,4 @@ eb init udagram-api --platform node.js --region us-east-1
 eb create --sample udagram-api-dev
 eb use udagram-api-dev
 eb setenv POSTGRES_USERNAME="postgres" POSTGRES_PASSWORD="postgres" POSTGRES_DB="postgres" POSTGRES_HOST="postgres.cxxd4kjd5ux0.us-east-1.rds.amazonaws.com" DB_PORT="5432" PORT="8080" JWT_SECRET="nemoone" AWS_ACCESS_KEY_ID="AKIA3RSDEFRPJANQNA5P" AWS_SECRET_ACCESS_KEY="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" AWS_REGION="us-east-1" AWS_BUCKET="nemo-bucket"
-eb depoly udagram-api-dev
+eb depoly 
